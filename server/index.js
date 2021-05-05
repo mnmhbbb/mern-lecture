1dc6a026d6584fb744c69b69808fccb2ccbde674
mnmhbbb <mnmhb64@gmail.com>
2021-05-05 20:01:55 +0900

    mongoose 설치 및 연결

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const config = require('./config/key');
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
