5db69dc7686005140626b841c4e9a6c14921e039
mnmhbbb <mnmhb64@gmail.com>
2021-05-06 15:28:15 +0900

    backend 로그인 설정 완료

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 const config = require('./config/key');
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 const mongoose = require('mongoose');
 mongoose
   .connect(config.mongoURI, {
@@ -13,9 +16,11 @@ mongoose
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use('/api/users', require('./routes/users'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
