e5ee4af446bcb4e8a1d48c08a5dc71fbd2338ca4
mnmhbbb <mnmhb64@gmail.com>
2021-05-11 05:25:42 +0900

    https://mern-usedshop.herokuapp.com:5000/으로 수정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://mern-usedshop.herokuapp.com:5000',
       changeOrigin: true,
     }),
   );
