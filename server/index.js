9b8b57d9108369afd6adc32d73377cd77b39d831
mnmhbbb <mnmhb64@gmail.com>
2021-05-14 15:30:59 +0900

    cors

--- server/index.js
+++ server/index.js
@@ -34,15 +34,15 @@ mongoose
 //   }
 // };
 
-module.exports = connectDB;
+// module.exports = connectDB;
 
 app.use(cors());
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     credentials: true,
-//   }),
-// );
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
