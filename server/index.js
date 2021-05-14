cc62d95204e67e0ed671f36dc674c023cdaf075a
mnmhbbb <mnmhb64@gmail.com>
2021-05-15 04:59:39 +0900

    heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-// require('dotenv').config({ path: './.env' });
 const config = require('./config/key');
 const cors = require('cors');
 
@@ -17,24 +16,6 @@ mongoose
   })
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useFindAndModify: true,
-//       useUnifiedTopology: true,
-//       useCreateIndex: true,
-//     });
-
-//     console.log('MongoDB Connection Success 👍');
-//   } catch (error) {
-//     console.log('MongoDB Connection Failed 💥');
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
 
 app.use(cors());
 app.use(
