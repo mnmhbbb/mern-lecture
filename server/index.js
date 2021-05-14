f5442f7c50364bf7fa4fa95517b1c1abebc2a2ce
mnmhbbb <mnmhb64@gmail.com>
2021-05-14 13:58:56 +0900

    test

--- server/index.js
+++ server/index.js
@@ -1,38 +1,38 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config({ path: './.env' });
-// const config = require('./config/key');
+// require('dotenv').config({ path: './.env' });
+const config = require('./config/key');
 const cors = require('cors');
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-// const mongoose = require('mongoose');
-// mongoose
-//   .connect(config.mongoURI, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log('MongoDB Connected...'))
-//   .catch((err) => console.log(err));
+const mongoose = require('mongoose');
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useFindAndModify: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useFindAndModify: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//     });
 
-    console.log('MongoDB Connection Success 👍');
-  } catch (error) {
-    console.log('MongoDB Connection Failed 💥');
-    process.exit(1);
-  }
-};
+//     console.log('MongoDB Connection Success 👍');
+//   } catch (error) {
+//     console.log('MongoDB Connection Failed 💥');
+//     process.exit(1);
+//   }
+// };
 
 module.exports = connectDB;
 
